ae52e6ae34009b2abc12f9ae928f20d81e4bc7cc
Shay O'Connell <shay.oconnell7@gmail.com>
2020-12-14 12:49:50 -0500

    built table with sighting information

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,32 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+// Making a table
+
+var tbody = d3.select("tbody");
+
+console.log(tableData)
+
+data.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+
+    Object.entries(sighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+      });
+  });
+
+
+
+// using a form to filter by date
+// var form = d3.select("#form")
+
+// form.on("submit", makeTable())
+
+// function makeTable() {
+
+// }
